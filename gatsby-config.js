b1626c0700ad7c34d3fd7582743d9e505c3e9b06
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-01-19 15:23:30 -0500

    yes we can use an external settings.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
+const settings = require('./settings.json')
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: settings.title,
+    description: settings.description,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
