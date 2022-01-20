34bff8b06f6629e3b18997f5ea7ea7039ac9bb62
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-01-20 16:21:24 -0500

    feat: automatically redirect to preferred locale

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 
 import useLocales from "../hooks/useLocales"
 import Layout from "../components/layout"
 
 const IndexPage = () => {
   const locales = useLocales()
+
+  React.useEffect(() => {
+    const preferredLocale = window.navigator.language
+    let matchingLocale = locales.find(
+      l => l.id.replace("_", "-") === preferredLocale
+    )
+    if (!matchingLocale)
+      matchingLocale = locales.find(
+        l => l.id === preferredLocale.replace(/-[A-Z]{2}/, "")
+      )
+
+    if (matchingLocale) {
+      console.log({ locales, preferredLocale, matchingLocale })
+      navigate(`/${matchingLocale.id}/`)
+    }
+  }, [])
+
   return (
     <Layout>
       {locales.map(({ id, i18n }) => (
