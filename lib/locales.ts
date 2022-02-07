9125e7b4f82ecdb8f24e504b64c2542326b5b2ef
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-02-07 11:15:50 -0500

    feat: fully type-check locale JSON files
    This adds more strict and complete type-checking when parsing the locale
    JSON files.
    
    First of all, it uses `const json: unknown = JSON.parse(fileContents)`,
    since `JSON.parse` by default returns `any`, since its implementation in
    TS predates the existence of the `unknown` type. This is more accurate,
    and helps us keep our type-checking more complete.
    
    Second, this change sets up an `i18nFields` in value-space, so we can
    also use a TS mapped type to get equivalents in type-space. That's the
    `[K in typeof i18nFields[number]]` stuff. See a full explanation here:
    https://codyarose.com/blog/object-keys-from-array-in-typescript/

--- lib/locales.ts
+++ lib/locales.ts
@@ -1,14 +1,18 @@
 import fs from "fs"
 import path from "path"
 
-export interface I18nFields {
-  view_in: string
-  lang_picker: string
-  title: string
-  description: string
-  hero_title: string
-  hero_body: string
-  hero_cta: string
+const i18nFields = [
+  "view_in",
+  "lang_picker",
+  "title",
+  "description",
+  "hero_title",
+  "hero_body",
+  "hero_cta",
+] as const
+
+export type I18nFields = {
+  [K in typeof i18nFields[number]]: string
 }
 
 export interface Locale {
@@ -29,22 +33,43 @@ function alphabeticOrder({ id: a }: Locale, { id: b }: Locale): -1 | 0 | 1 {
 
 const localesDirectory = path.join(process.cwd(), "i18n")
 
-function checkI18nFields(fileName: string, data: { [key: string]: any }) {
-  ;[
-    "view_in",
-    "lang_picker",
-    "title",
-    "description",
-    "hero_title",
-    "hero_body",
-    "hero_cta",
-  ].forEach(attr => {
-    if (!data.hasOwnProperty(attr)) {
-      throw new Error(`Expected ${fileName} to have field "${attr}"`)
+/**
+ * Returns something close to `I18nFields`, but with the type of each field being `unknown`
+ *
+ * This and the subsequent check that each field is a string can be combined
+ * into a single loop once TS implements `in` as a proper type guard:
+ * https://github.com/microsoft/TypeScript/issues/21732
+ */
+function isI18nShape(
+  fileName: string,
+  obj: {}
+): obj is { [K in typeof i18nFields[number]]: unknown } {
+  i18nFields.forEach(field => {
+    if (!(field in obj)) {
+      console.error(`Expected ${fileName} to have field "${field}"`)
+      return false
+    }
+  })
+  return true
+}
+function checkI18nFields(fileName: string, fileContents: string): I18nFields {
+  const json: unknown = JSON.parse(fileContents)
+  if (
+    typeof json !== "object" ||
+    json === null ||
+    !isI18nShape(fileName, json)
+  ) {
+    throw new Error(
+      `Expected ${fileName} to contain an object with keys: ${i18nFields}`
+    )
+  }
+  i18nFields.forEach(attr => {
+    if (typeof json[attr] !== "string") {
+      throw new Error(`Expected ${fileName} to have string field "${attr}"`)
     }
   })
 
-  return data as I18nFields
+  return json as I18nFields
 }
 
 export function getLocales(): Locale[] {
@@ -60,7 +85,7 @@ export function getLocales(): Locale[] {
     const id = fileName.replace(/\.json$/, "")
     const fullPath = path.join(localesDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, "utf8")
-    const i18n = checkI18nFields(fileName, JSON.parse(fileContents))
+    const i18n = checkI18nFields(fileName, fileContents)
 
     // Combine the data with the id
     return {
@@ -76,7 +101,7 @@ export async function getLocale(id: string): Promise<Locale> {
   const fileName = `${id}.json`
   const fullPath = path.join(localesDirectory, fileName)
   const fileContents = fs.readFileSync(fullPath, "utf8")
-  const i18n = checkI18nFields(fileName, JSON.parse(fileContents))
+  const i18n = checkI18nFields(fileName, fileContents)
 
   return {
     id,
