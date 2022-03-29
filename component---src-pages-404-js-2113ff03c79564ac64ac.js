c7170557754ddd4427363d8c1c2ff905befcd3f0
chadoh <221614+chadoh@users.noreply.github.com>
2022-03-29 15:30:16 +0000

    Deploying to gh-pages from  @ da9302ee9f8dd8f836e02d89d9deb83e928fa6be 🚀

---
+++ component---src-pages-404-js-2113ff03c79564ac64ac.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend_starter=self.webpackChunkfrontend_starter||[]).push([[883],{9616:function(e,t,n){n.r(t);var r=n(7294),u=n(4045),l=n(8972);t.default=function(){return r.createElement(u.Z,null,r.createElement(l.Z,{title:"404: Not found"}),r.createElement("h1",null,"404: Not Found"),r.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2113ff03c79564ac64ac.js.map
