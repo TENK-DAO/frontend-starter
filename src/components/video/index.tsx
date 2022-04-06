62235f9881dcd4cf7a875858ee3cc04f78fa6f38
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-04-06 12:01:02 -0400

    fix: rm VP9 video because Safari is horrible

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -10,10 +10,9 @@ export type VideoProps = React.DetailedHTMLProps<
 }
 
 const Video: React.FC<VideoProps> = ({ src, ...props }) => {
-  const { videoH264, videoVP9 } = useVideoData(src)
+  const { videoH264 } = useVideoData(src)
   return (
     <video playsInline muted preload="auto" {...props}>
-      <source src={withPrefix(videoVP9.path!)} type="video/webm; codecs=vp9,opus" />
       <source src={withPrefix(videoH264.path!)} type="video/mp4; codecs=avc1" />
     </video>
   )
