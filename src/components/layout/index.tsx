42ac4177f0ba7fbaa94b221546d0f1fd0fdce78a
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-04-14 11:48:38 -0400

    feat: move social and langpicker to footer

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,14 +1,19 @@
 import * as React from "react"
 import Banner from "../banner"
 import Nav from "../nav"
+import Footer from "../footer"
 import "./layout.scss"
+import * as css from "./layout.module.css"
 
 const Layout: React.FC<{ style?: React.CSSProperties }> = ({ style, children }) => {
   return (
-    <div style={style}>
-      <Banner />
-      <Nav />
-      {children}
+    <div style={style} className={css.wrap}>
+      <div>
+        <Banner />
+        <Nav />
+        {children}
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -17,18 +17,10 @@ export default function Nav() {
   const { locale } = useLocales()
   if (!locale) return null
   return (
-    <nav className={css.nav}>
+    <nav className={`${css.nav} container`}>
       <h1 className={css.title}>
         {locale.title}
       </h1>
-      <div className={css.social}>
-        {settings.social.map(({ href, img, alt }) => (
-          <a href={href} target="_blank" rel="noopener noreferrer" title={alt} key={alt}>
-            <Image src={img} alt={alt} />
-          </a>
-        ))}
-      </div>
-      <LangPicker />
       {currentUser ? (
         <span>
           {/* extra span so that Gatsby's hydration notices this is not the same as the signIn button */}
