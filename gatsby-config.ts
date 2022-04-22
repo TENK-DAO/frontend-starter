0fad81e15af784a3f1dfdb772ca02c10480cb678
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-04-22 16:26:12 -0400

    feat: combine My NFTs & Reveal, v1
    This will eventually have nicer animations, but for now it shows the
    word "New!" underneath the newly-added NFTs in your collection. Clicking
    an NFT still opens the Lightbox in an overlay-over-the-overlay.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -65,6 +65,7 @@ const config: GatsbyConfig = {
       },
     },
     `gatsby-plugin-sass`,
+    `gatsby-plugin-portal`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
